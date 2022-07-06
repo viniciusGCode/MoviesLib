1510b4bc0d15d28158f3a18ec9bbcdbb05b596cd
UT0P1C <ljonham@gmail.com>
2022-07-06 18:23:36 -0300

    finalizando o sistema de busca

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -5,17 +5,36 @@ import MovieCard from "../components/MovieCard/MovieCard";
 
 import "../components/MovieCard/MovieCard.css"
 
-const searchURL = import.meta.env.VITE_SEARCH;
-const apiKey = import.meta.env.VITE_API_KEY;
 
 
 
 const Search = () => {
+	const searchURL = import.meta.env.VITE_SEARCH;
+	const apiKey = import.meta.env.VITE_API_KEY;
 
 	const [searchParams] = useSearchParams();
 
 	const [movies, setMovies] = useState([]);
 	const query = searchParams.get("q");
+
+
+
+	const getSearchedMovies = async (url) => {
+
+		const res = await fetch(url);
+		const data = await res.json();
+
+		setMovies(data.results);
+		
+	}
+
+	useEffect(() => {
+
+		const searchedMoviesUrl = `${searchURL}${apiKey}&query=${query}&language=pt-BR`;
+
+		getSearchedMovies(searchedMoviesUrl);
+
+	}, [query]);
 
 
 	return (
@@ -28,7 +47,9 @@ const Search = () => {
 				</div>
 
 				<div className="movies-container">
-
+					{
+						movies.length > 0 && movies.map((movie) => <MovieCard movie={movie} key={movie.id}/> )
+					}
 				</div>
 			</div>
 		</div>
